5c3d4d8456553a176c2c2786b2b19cf23a28c760
Lucas G. Oliveira <lucassantosdosreis369@gmail.com>
2026-09-06 09:37:57 -0300

    Add files via upload

--- 03-09-2026 (1)/q1.js
+++ 03-09-2026 (1)/q1.js
@@ -2,19 +2,28 @@
 
 const v = [];
 let soma = 0;
+let indice = 0;
 
 
 for(let i = 1 ; i <= 5; i ++){
-    v(i) = number(prompt(`Digite o valor do ${i}º produto `));
 
-    if()
+    v.push(Number(prompt(`Digite o valor do ${i}º produto `)));
+
 }
 
-for(let valor of v){
-    soma += valor
+let maiorpre = v[0];
+
+for(let i = 0; i < v.length; i ++){
+    soma += v[i]
+
+    if(v[i] > maiorpre) {
+        maiorpre = v[i];
+        indice = i;
+    }
 }
 
 console.log(`A soma dos valores é ${soma}`);
+console.log(`O índice do produto mais caro é ${indice} com o valor de ${maiorpre} `);
 
 
 
