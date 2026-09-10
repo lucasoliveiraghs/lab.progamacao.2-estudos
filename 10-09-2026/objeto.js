2b9c21156c27ce3650bceebbb1c31a3eccda7a64
Lucas G. Oliveira <lucassantosdosreis369@gmail.com>
2026-09-10 15:39:27 -0300

    Add files via upload

---
+++ 10-09-2026/objeto.js
@@ -0,0 +1,11 @@
+const automovel1 = {
+    nome: prompt("Digite o nome do primeiro automóvel"),
+    modelo: prompt("Digite o modelo do primeiro auomóvel"),
+    marca: prompt("Digite a marca do primeiro automóvel"),
+
+}
+const automovel2 = {
+    nome: prompt("Digite o nome do segundo automóvel"),
+    modelo: prompt("Digite o modelo do segundo auomóvel"),
+    marca: prompt("Digite a marca do segundo automóvel"),
+}
