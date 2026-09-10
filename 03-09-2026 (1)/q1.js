deb8fdef5762f3fe8ad2a468ebb771fe82742e0b
Lucas G. Oliveira <lucassantosdosreis369@gmail.com>
2026-09-10 15:15:04 -0300

    Refactor product price calculation logic

--- 03-09-2026 (1)/q1.js
+++ 03-09-2026 (1)/q1.js
@@ -1,19 +1,21 @@
 /*Atividade - Faça um programa que crie e leia 5 valores de preços de produtos de uma loja. Após a leitura desses valores, percorra o vetor e imprima a soma de valores desses produtos. Além disso, imprima também o índice onde está o produto mais caro.*/
 
 const v = [];
+const tamanho = 5;
 let soma = 0;
 let indice = 0;
 
 
-for(let i = 1 ; i <= 5; i ++){
+for(let i = 0 ; i < tamanho; i ++){
 
-    v.push(Number(prompt(`Digite o valor do ${i}º produto `)));
+    let preco = (Number(prompt(`Digite o valor do ${i}º produto `)));
+    v.push(preco);
 
 }
 
 let maiorpre = v[0];
 
-for(let i = 0; i < v.length; i ++){
+for(let i = 0; i < tamanho; i ++){
     soma += v[i]
 
     if(v[i] > maiorpre) {
